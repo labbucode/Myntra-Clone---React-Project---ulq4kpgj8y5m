af01eda501eabfef949aab6526e38d2f1d660c34
Laraib <syedlaraib745@gmail.com>
2023-05-31 10:52:18 +0530

    edit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,49 +1,80 @@
-import "../Styles/Cart.css"
+import React from 'react';
+import "../Styles/Cart.css";
 
-export default function CartItems({ cartItems, setCartItems }){
-    function handleDelete(data) {
-        const newCartItems = cartItems.filter(item => item.id != data.id)
-        setCartItems([...newCartItems])
-        localStorage.setItem('datas', JSON.stringify([...newCartItems]))
-    }
- 
-    return (
-        <>
-            {cartItems.length === 0 ? (
-                <p className="emptyCart">Your Cart is Empty</p>
-            ) : (
-                    <>
-                <ul className="cartsection">
-                    {cartItems.map((item) => {
-                        return (
-                            <li key={item.id} className="cartproduct">
-                                <div className="cartImage">
-                                    <img src={item.otherImages[0]} alt="" />
-                                </div>
-                                <div></div>
-                                <div className="cartdescription">
-                                    <h1>{item.name}</h1>
-                                    <p>{item.description}</p>
-                                    <p className="price">
-                                        <b>rs:</b>
-                                        <s>{item.strickPrice}</s>
-                                        <span> {item.discount}% {item.finalPrice}</span>
-                                    </p>
-                                    <p className="size">size:</p>
-                                    {item.productSize}
-                                    <br />
-                                    <button>Buy now</button>
-                                    <button onClick={() => handleDelete(item)}>
-                                        Remove
-                                    </button>
-                                </div>
-                            </li>
-                        );
-                    })}
-                </ul>
-                   
-                    </>
-            )}
-        </>
-    );
+export default function CartItems({ cartItems, setCartItems }) {
+ function handleDelete(data) {
+  const newCartItems = cartItems.filter(item => item.id !== data.id);
+  setCartItems(newCartItems);
+  localStorage.setItem('datas', JSON.stringify(newCartItems));
+ }
+
+ function handleQuantityIncrease(item) {
+  const updatedCartItems = cartItems.map(cartItem => {
+   if (cartItem.id === item.id) {
+    return {
+     ...cartItem,
+     Quantity: cartItem.Quantity + 1
+    };
+   }
+   return cartItem;
+  });
+  setCartItems(updatedCartItems);
+  localStorage.setItem('datas', JSON.stringify(updatedCartItems));
+ }
+
+ function handleQuantityDecrease(item) {
+  const updatedCartItems = cartItems.map(cartItem => {
+   if (cartItem.id === item.id && cartItem.Quantity > 1) {
+    return {
+     ...cartItem,
+     Quantity: cartItem.Quantity - 1
+    };
+   }
+   return cartItem;
+  });
+  setCartItems(updatedCartItems);
+  localStorage.setItem('datas', JSON.stringify(updatedCartItems));
+ }
+
+ return (
+  <>
+   {cartItems.length === 0 ? (
+    <p className="emptyCart">Your Cart is Empty</p>
+   ) : (
+    <>
+     <ul className="cartsection">
+      {cartItems.map((item) => {
+       return (
+        <li key={item.id} className="cartproduct">
+         <div className="cartImage">
+          <img src={item.otherImages[0]} alt="" />
+         </div>
+         <div></div>
+         <div className="cartdescription">
+          <h1>{item.name}</h1>
+          <p>{item.description}</p>
+          <p className="price">
+           <b>rs:</b>
+           <s>{item.strickPrice}</s>
+           <span> {item.discount}% {item.finalPrice}</span>
+          </p>
+          <p className="size">size:</p>
+          {item.productSize}
+          <p className="quantity">
+           Quantity: {item.Quantity}
+           <button onClick={() => handleQuantityIncrease(item)}>+</button>
+           <button onClick={() => handleQuantityDecrease(item)}>-</button>
+          </p>
+          <br />
+          <button>Buy now</button>
+          <button onClick={() => handleDelete(item)}>Remove</button>
+         </div>
+        </li>
+       );
+      })}
+     </ul>
+    </>
+   )}
+  </>
+ );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,57 +1,72 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import data from "../data";
-import "../Styles/Product.css"
-
+import "../Styles/Product.css";
 
 export default function Product({ cartItems, setCartItems }) {
+  const { id } = useParams();
+  const clickedItem = data.find((item) => item.id === Number(id));
+  const [quantity, setQuantity] = useState(1);
 
-
-
-  const { id } = useParams()
-  const clickedItem = data.find(item => item.id === Number(id))
   function handleCart(cartData) {
-
-    setCartItems([...cartItems, cartData])
-    localStorage.setItem('datas', JSON.stringify([...cartItems, cartData]))
+    const existingItem = cartItems.find((item) => item.id === cartData.id);
+    if (existingItem) {
+      const updatedCartItems = cartItems.map((item) => {
+        if (item.id === cartData.id) {
+          return { ...item, Quantity: item.Quantity + quantity };
+        }
+        return item;
+      });
+      setCartItems(updatedCartItems);
+    } else {
+      setCartItems([...cartItems, { ...cartData, Quantity: quantity }]);
+    }
+    localStorage.setItem("datas", JSON.stringify([...cartItems, cartData]));
   }
 
-  function handleSize(event){
+  function handleSize(event) {
     return event;
   }
 
   return (
-
-    < div style={{ display: "flex", justifyContent: "center" }}>
-      <div className='prod-photo'>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div className="prod-photo">
         <img src={clickedItem.otherImages[0]} />
         <img src={clickedItem.otherImages[1]} />
         <img src={clickedItem.otherImages[2]} />
         <img src={clickedItem.otherImages[3]} />
       </div>
-
-
-      <div className='prod-modal'>
-        <div className='prod-detail'>
-          <h3 >{clickedItem.name}</h3>
-          <em >{clickedItem.description}</em>
+      <div className="prod-modal">
+        <div className="prod-detail">
+          <h3>{clickedItem.name}</h3>
+          <em>{clickedItem.description}</em>
           <p style={{ marginTop: "15%" }}>Rs.{clickedItem.finalPrice}</p>
-          <p style={{ textDecoration: "line-through" }}>Rs.{clickedItem.strickPrice}</p>
-          <strong style={{ color: "red" }}>{clickedItem.discount} %OFF</strong>
+          <p style={{ textDecoration: "line-through" }}>
+            Rs.{clickedItem.strickPrice}
+          </p>
+          <strong style={{ color: "red" }}>{clickedItem.discount}% OFF</strong>
           <p>Size chart</p>
-          <span className='prod-size'>
+          <span className="prod-size">
             <span onClick={handleSize}>{clickedItem.productSize.slice(0, 2)}</span>
             <span onClick={handleSize}>{clickedItem.productSize.slice(4, 6)}</span>
             <span onClick={handleSize}>{clickedItem.productSize.slice(8, 10)}</span>
             <span onClick={handleSize}>{clickedItem.productSize.slice(12, 14)}</span>
           </span>
+          <div className="quantity">
+            <label htmlFor="quantity">Quantity:</label>
+            <input
+              type="number"
+              id="quantity"
+              min="1"
+              value={quantity}
+              onChange={(e) => setQuantity(parseInt(e.target.value))}
+            />
+          </div>
         </div>
-
-        <button onClick={() => handleCart(clickedItem)} className='cartBtn'>
+        <button onClick={() => handleCart(clickedItem)} className="cartBtn">
           Add to Cart
         </button>
       </div>
     </div>
-  )
-
-
+  );
 }
